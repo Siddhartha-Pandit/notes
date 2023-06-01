ba3909bd0a7468dce524e02eced17dec53f5a910
Siddhartha <Siddharthapandit66@gmail.com>
2023-06-01 15:36:01 +0530

    __str__ added in models

--- notes/noterecord/models.py
+++ notes/noterecord/models.py
@@ -5,11 +5,17 @@ class notedata(models.Model):
     title=models.CharField(max_length=100)
     detail=models.CharField(max_length=500)
 
+    def __str__(self):
+        return self.title
+
 class todo(models.Model):
     id=models.AutoField
     todolist=models.CharField(max_length=256)
     time=models.DateTimeField()
     status=models.BooleanField()
+
+    def __str__(self):
+        return self.todolist
 
 
 
